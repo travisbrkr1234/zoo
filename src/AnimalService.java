3ceee17c85fc0367991e026a74403b7b46a44b33
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2016-04-28 12:45:42 -0700

    add animal service, use in program

---
+++ src/AnimalService.java
@@ -0,0 +1,40 @@
+/**
+ * Created by carlos.ochoa on 4/28/2016.
+ */
+public class AnimalService {
+    //create animal in DB with data collected from user input
+    public static void createAnimal() {
+
+        Animal animal = new Animal();
+
+        boolean stopLoop = false;
+        boolean stopLoop2 = false;
+
+        while (!stopLoop) {
+            System.out.println("Please enter an animal name ex... 'Larry'");
+            String animalName = GetUserInput.getAnimalName();
+            boolean validAnimalName = Validation.validateAnimalName(animalName);
+            if (validAnimalName) {
+                animal.setName(animalName);
+                stopLoop = true;
+            }
+        }
+
+        while (!stopLoop2) {
+            System.out.println("Please enter an animal age ex... 41");
+            int animalAge = GetUserInput.getAnimalAge();
+            boolean validAnimalAge = Validation.validateAnimalAge(animalAge);
+            if (validAnimalAge) {
+                animal.setAge(animalAge);
+                stopLoop2 = true;
+            }
+        }
+
+        JDBCAnimalDAO jdbcAnimalDAO = new JDBCAnimalDAO();
+        jdbcAnimalDAO.getConnection();
+        jdbcAnimalDAO.insert(animal); //
+        jdbcAnimalDAO.select(); //show animals after inserting one
+        jdbcAnimalDAO.closeConnection();
+    }
+
+}
